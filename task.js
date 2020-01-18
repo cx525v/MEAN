e98eb95a1f9f3b4ce7bf06844e35e60ead6a63da
cx525v <cx525v@gmail.com>
2020-01-18 17:08:57 -0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        minlength: 3
+    },
+
+    _listId: {
+       type: mongoose.Types.ObjectId,
+       required: true
+    },
+
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = Task;
